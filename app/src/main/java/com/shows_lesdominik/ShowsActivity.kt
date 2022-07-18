6203e423d920ec7a0fe8c7f7d889e5a82d8bc201
lesdominik <les.dominik@gmail.com>
2022-07-18 19:44:55 +0200

    attach the adapter to the RecyclerView

--- app/src/main/java/com/shows_lesdominik/ShowsActivity.kt
+++ app/src/main/java/com/shows_lesdominik/ShowsActivity.kt
@@ -2,17 +2,39 @@ package com.shows_lesdominik
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.shows_lesdominik.databinding.ActivityShowsBinding
+import model.Show
+import ui.ShowsAdapter
 
 class ShowsActivity : AppCompatActivity() {
 
-    lateinit var binding: ActivityShowsBinding
+    private val shows = listOf(
+        Show("The Office", R.drawable.the_office),
+        Show("Stranger Things", R.drawable.stranger_things),
+        Show("Krv nije voda", R.drawable.krv_nije_voda)
+    )
+
+    private lateinit var binding: ActivityShowsBinding
+    private lateinit var adapter: ShowsAdapter
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
         binding = ActivityShowsBinding.inflate(layoutInflater)
-
         setContentView(binding.root)
+
+        initShowsRecycler()
+    }
+
+    private fun initShowsRecycler() {
+        adapter = ShowsAdapter(shows) { show ->
+            Toast.makeText(this, show.name, Toast.LENGTH_SHORT).show()
+        }
+
+        binding.showsRecycler.layoutManager = LinearLayoutManager(this)
+
+        binding.showsRecycler.adapter = adapter
     }
 }
