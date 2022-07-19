7f7e20d88847ceeae54a80b806c32467f1ff8f9c
lesdominik <les.dominik@gmail.com>
2022-07-19 14:31:40 +0200

    implement 'write a review' BottomSheetDialog

--- app/src/main/java/com/shows_lesdominik/ShowDetailsActivity.kt
+++ app/src/main/java/com/shows_lesdominik/ShowDetailsActivity.kt
@@ -3,8 +3,11 @@ package com.shows_lesdominik
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.LayoutInflater
+import com.google.android.material.bottomsheet.BottomSheetDialog
 import com.shows_lesdominik.databinding.ActivityShowDetailsBinding
 import com.shows_lesdominik.databinding.ActivityShowsBinding
+import com.shows_lesdominik.databinding.DialogAddReviewBinding
 
 class ShowDetailsActivity : AppCompatActivity() {
 
@@ -23,5 +26,33 @@ class ShowDetailsActivity : AppCompatActivity() {
             val intent = Intent(this, ShowsActivity::class.java)
             startActivity(intent)
         }
+
+        binding.reviewButton.setOnClickListener {
+            showAddReviewBottomSheet()
+        }
+    }
+
+    private fun showAddReviewBottomSheet() {
+        val dialog = BottomSheetDialog(this)
+
+        val bottomSheetBinding = DialogAddReviewBinding.inflate(layoutInflater)
+        dialog.setContentView(bottomSheetBinding.root)
+
+        bottomSheetBinding.closeButton.setOnClickListener {
+            dialog.hide()
+        }
+
+        bottomSheetBinding.showRatingBar.setOnRatingBarChangeListener { ratingBar, rating, b ->
+            when {
+                rating > 0 -> {
+                    bottomSheetBinding.submitButton.isEnabled = true
+                }
+                else -> {
+                    bottomSheetBinding.submitButton.isEnabled = false
+                }
+            }
+        }
+
+        dialog.show()
     }
 }
